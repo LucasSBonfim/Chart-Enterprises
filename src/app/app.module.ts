edef88d017efacc39014d504367f1dda515d9e2a
ivanrogeriio <ivanpassosfilho@hotmail.com>
2021-03-20 17:14:42 -0300

    modifing components and archives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,42 @@
+
+/* Angular dependencies (auto installed) */
+
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { ChartsComponent } from './charts/charts.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
+
+/* Components imports */
+
+import { ChartsComponent } from './components/charts/charts.component';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @NgModule({
+
   declarations: [
+
     AppComponent,
     ChartsComponent,
     NavbarComponent
+
   ],
+
   imports: [
+
     BrowserModule,
     AppRoutingModule
+
   ],
+
   providers: [],
   bootstrap: [AppComponent]
+
 })
+
 export class AppModule { }
